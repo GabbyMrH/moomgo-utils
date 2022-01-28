32761da4247e61aa0f524fcc65f8a96caefbb901
“casso” <1152283574@qq.com>
2022-01-28 12:00:08 +0800

    docs(README):添加提交规范文档

--- copy/copy.go
+++ copy/copy.go
@@ -4,7 +4,23 @@
  * @Author: Casso
  * @Date: 2022-01-28 11:11:23
  * @LastModifiedBy: Casso
- * @LastEditTime: 2022-01-28 11:11:23
+ * @LastEditTime: 2022-01-28 11:33:06
  */
 
 package copy
+
+import "reflect"
+
+// CopyStruct 复制两个结构体内相同字段
+func CopyStruct(src, dst *reflect.Value) *reflect.Value {
+	for i := 0; i < src.NumField(); i++ {
+		filed := src.Type().Field(i).Name
+		cfiled := dst.FieldByName(filed)
+		// 可用的属性且可 赋值
+		if cfiled.IsValid() && cfiled.CanSet() {
+			dst.Field(i).Set(src.Field(i))
+		}
+	}
+
+	return dst
+}
--- copy/copy_test.go
+++ copy/copy_test.go
@@ -4,7 +4,37 @@
  * @Author: Casso
  * @Date: 2022-01-28 11:13:22
  * @LastModifiedBy: Casso
- * @LastEditTime: 2022-01-28 11:13:22
+ * @LastEditTime: 2022-01-28 11:57:23
  */
 
 package copy
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCopyStruct(t *testing.T) {
+	type Person struct {
+		Name string
+		Age  int64
+	}
+
+	type Stu struct {
+		Name string
+		Age  int64
+	}
+
+	var A = Person{
+		Name: "casso",
+		Age:  20,
+	}
+	var B Stu
+	dst := reflect.ValueOf(&A).Elem()
+	src := reflect.ValueOf(&B).Elem()
+	B2 := CopyStruct(&dst, &src).Interface().(Stu)
+
+	if A.Name != B2.Name || A.Age != B2.Age {
+		t.Errorf("want: %#v; got: %#v", A, B)
+	}
+}
